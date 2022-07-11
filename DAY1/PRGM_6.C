06147e44927e77def28ab2dd71a6fdc078026e65
RakeshNarthu <108972895+RakeshNarthu@users.noreply.github.com>
2022-07-11 17:09:31 +0530

    Create PRGM_6.C

---
+++ DAY1/PRGM_6.C
@@ -0,0 +1,11 @@
+Students with roll numbers 1-9 are seated in ascending order in an examination hall.
+  There has been a slight mistake in their seating arrangement where two consecutive roll numbers have been swapped.
+  Choose these two consecutive numbers as per your choice and swap them back in their right places according to ascending order.
+  
+  #include<stdio.h>
+  int i;
+printf("enter value");
+scanf("%d",&i);
+for(i=1;i<=9;i++){
+  printf("%d",i);
+}
