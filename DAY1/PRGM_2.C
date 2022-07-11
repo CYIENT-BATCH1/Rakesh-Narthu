292a138801cae8864d6142becb23fb1f83615067
RakeshNarthu <108972895+RakeshNarthu@users.noreply.github.com>
2022-07-11 16:10:40 +0530

    Create PRGM_2.C

---
+++ DAY1/PRGM_2.C
@@ -0,0 +1,11 @@
+2. The entire area of a circular garden is to be covered with grass by a Gardener. The radius of the garden is given by the user input. Calculate the area that has to be covered.
+
+    #include<stdio.h>
+    int main()
+    {
+    float r,n;
+    printf("enter the r value");
+    scanf("%f",&r);
+    n=3.14*r*r;
+    printf("%f",n);
+    }
